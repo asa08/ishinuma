e270d5cd987dc5d8360f4485705a7f7e7f0a75a3
maiko-m <mn.6182.nm@gmail.com>
2021-09-21 15:17:39 +0900

    :sparkles: init

---
+++ ishinuma/Screen/Feed/Builder/FeedBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  FeedBuilder.swift
+//  FeedBuilder
+//
+//  Created by maiko morisaki on 2021/09/21.
+//
+
+import Foundation
+
+class FeedBuilder {
+    func build() -> FeedViewController {
+        let controller = FeedViewController()
+        return controller
+    }
+}
---
+++ ishinuma/Screen/Feed/View/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ishinuma
+//
+//  Created by maiko morisaki on 2021/09/15.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
