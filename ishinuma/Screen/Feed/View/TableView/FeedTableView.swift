d4aa64245c8739c59bbccea1dd3afe4dadc39fb0
maiko-m <mn.6182.nm@gmail.com>
2021-10-06 00:59:59 +0900

    :+1: init table

---
+++ ishinuma/Screen/Feed/View/TableView/FeedTableView.swift
@@ -0,0 +1,17 @@
+//
+//  FeedTableView.swift
+//  ishinuma
+//
+//  Created by maiko morisaki on 2021/10/06.
+//
+
+import UIKit
+
+class FeedTableView: UITableView {
+    
+    private var viewModel: FeedViewModel?
+    
+    func initSelf(viewModel: FeedViewModel) {
+        self.viewModel = viewModel
+    }
+}
---
+++ ishinuma/Screen/Feed/View/TableView/FeedTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedTableViewCell.swift
+//  ishinuma
+//
+//  Created by maiko morisaki on 2021/10/06.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    
+    func initSelf(store: StoreModel) {
+        
+    }
+}
