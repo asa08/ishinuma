3f2a9d036c119df62ebfc24d4303e58ef1e72c01
maiko-m <mn.6182.nm@gmail.com>
2021-10-17 23:14:02 +0900

    :+1: info viewのbaseを作成

--- ishinuma/Screen/Feed/View/TableView/FeedTableViewCellInfos.swift
+++ ishinuma/Screen/Feed/View/TableView/FeedTableViewCellInfos.swift
@@ -10,4 +10,49 @@ import UIKit
 
 class FeedTableViewCellInfos: UIView {
     
+    private let baseView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        return view
+    }()
+    
+    init(_ store: StoreInfoModel) {
+        super.init(frame: .zero)
+        commonInit(store)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func commonInit(_ store: StoreInfoModel) {
+        clearTranslates()
+        makeAutolayout()
+    }
+    
+    private func makeAutolayout() {
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
+    private func clearTranslates(filter: ((UIView) -> Bool)? = nil) {
+        clearTranslates(self, filter: filter)
+    }
+    
+    private func clearTranslates(_ view: UIView, filter: ((UIView) -> Bool)?) {
+        if filter == nil || filter?(view) == true {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            view.subviews.forEach({ clearTranslates($0, filter: filter) })
+        }
+    }
 }
